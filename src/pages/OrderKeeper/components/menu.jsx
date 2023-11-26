0eb0088999017c92b7c706b7d0ca83698a787b91
dinarsuley <suleydinar@gmail.com>
2023-11-26 14:54:12 +0500

    niggers

--- src/pages/OrderKeeper/components/menu.jsx
+++ src/pages/OrderKeeper/components/menu.jsx
@@ -2,17 +2,15 @@ import style from "../styles/menu.module.css"
 import "../styles/info.css"
 
 
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { deleteOrder, fetchAllProducts, postOrder, setPaidTable } from "../requests/requests"
 import { setClicked, setEditMode} from "../store/reducers/tablesSlice"
-import Modal from './modal';
 
 
 
 const Menu = (props) =>{
     const editMode = useSelector(state => state.tables.editMode)
-    const [active, setActive] = useState(false)
     const clicked = useSelector(state => state.tables.clicked)
     const tables = useSelector(state => state.tables.tables)
     const currentTableId = useSelector(state => state.tables.currentTableId)
@@ -47,11 +45,6 @@ const Menu = (props) =>{
             </div>
         )
     }
-    let sum = 0
-    let ordersList = table.orders.map(function(element, index){
-        sum += element.price
-        return <p>{element.name} - {element.price}</p>
-      })
     
 
     return(
@@ -60,15 +53,6 @@ const Menu = (props) =>{
                 clicked
                 ?
                 <div className={style.menu_container}>
-                    <Modal active={active} setActive={setActive}>
-                        <div className={style.schet}>
-                            Счет стола №{table.id}
-                            {ordersList}
-                            <p className={style.schet_sum}>Сумма - {sum}</p>
-                            <button className={style.schet_btn_ok} onClick={()=>setPaidTable(table.id, dispatch, setActive)}>Готово</button>
-                            <button className={style.schet_btn_back} onClick={()=>setActive(false)}>Отмена</button>
-                        </div>
-                    </Modal>
                     <p>Стол №{table.id}</p>
                     {
                         editMode
@@ -83,7 +67,7 @@ const Menu = (props) =>{
                             ?
                             <div className={style.menu_container}>
                                 <button className={style.button} onClick={()=>dispatch(setEditMode(true))}>Редактировать заказ</button>
-                                {table.orders.length > 0 && <button className={style.button} onClick={()=>setActive(true)}>Расчитать и закрыть стол</button>}
+                                {table.orders.length > 0 && <button className={style.button}>Расчитать и закрыть стол</button>}
                                 <button className={style.button} onClick={()=>dispatch(setClicked(false))}>Закрыть</button>
                             </div>
                             :
